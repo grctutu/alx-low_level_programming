0e139d5410cad12320831ae90e46c045b48da791
grctutu <grctutu@gmail.com>
2022-03-11 17:13:16 -0800

    single digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main- Printsall possible combinations of single digit numbers
+ * Return: 0 on success
+ */
+int main(void)
+{
+int i = '0';
+
+while (i <= '9')
+{
+putchar(i);
+if (i != '9')
+}
+i++;
+}
+putchar('\n');
+return(0);
+}
