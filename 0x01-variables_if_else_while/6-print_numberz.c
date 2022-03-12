645e846a2afc87c53c31aa3cd7b200ae20dfa8c8
grctutu <grctutu@gmail.com>
2022-03-11 16:35:32 -0800

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main- Prints numbers 0-9 without using char or printf/puts
+ * and putcher 2x max
+ * Return: 0 on success
+ */
+int main(void)
+{
+int i = '0';
+
+while (i <= '9)
+{
+putchar(i);
+i++;
+}
+putchar('\n');
+return(0);
+}
