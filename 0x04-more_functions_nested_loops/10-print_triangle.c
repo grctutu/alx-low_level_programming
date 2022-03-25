ef0a9fd5a0496f26a0234f8b89d91d6b1347c22e
grctutu <grctutu@gmail.com>
2022-03-25 10:29:13 -0700

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,19 +6,26 @@
  */
 void print_triangle(int size)
 {
-	int a, b;
-
 	if (size <= 0)
-		_putchar('\n');
-	for (a = 1; a <= size; a++)
 	{
-		for (b = 1; b <= size; b++)
-		{
-			if (b <= (size - a))
-				_putchar(' ');
-		else
-			_putchar('#');
-		}
 		_putchar('\n');
+	} else
+	{
+		int i, j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = i; j < size; j++)
+			{
+				_putchar(' ');
+			}
+
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
+		}
 	}
 }
