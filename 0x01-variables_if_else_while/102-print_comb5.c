779d35698d8221b0c16d3d7a59b72d777db0d909
grctutu <grctutu@gmail.com>
2022-03-25 01:51:43 -0700

    2 digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,25 +7,40 @@
  */
 int main(void)
 {
-int i, j;
+	int c, i, k, j;
 
-for (i = 0; i <= 99; i++)
-{
-if (i < j && i != j)
-{
-putchar((i / 10) + '0');
-putchar((i % 10) + '0');
-putchar(' ');
-putchar((j / 10) + '0');
-putchar((j % 10) + '0');
-if (i != 98 || j != 99)
-{
-putchar(',');
-putchar(' ');
+	for (c = 48; c <= 57; c++)
+	{
+		for (i = 48; i <= 57; i++)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+				for (j = 48; j <= 57; j++)
+				{
+					if (((k + j) > (c + i) &&  k >= c) || c < k)
+					{
+						putchar(c);
+						putchar(i);
+						putchar(' ');
+						putchar(k);
+						putchar(j);
+
+					if (c + i + k + j == 227 && c == 57)
+					{
+					break;
+					}
+					else
+					{
+					putchar(',');
+					putchar(' ');
+					}
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
 }
-}
-}
-}
-putchar('\n');
-return(0); }
- 
